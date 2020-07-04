4775d1f62716b80262552b2b7e7d5352cf394f4e
Subak.io <50165193+subakioinc@users.noreply.github.com>
2020-07-04 10:42:39 +0900

    Update pose_receiver.cpp

--- src/pose_receiver.cpp
+++ src/pose_receiver.cpp
@@ -1,47 +1,44 @@
 #include "ros/ros.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "tf/transform_datatypes.h"
 
-// to-do
-// subscribe topic : "hd_indoor/pose", type: geomety_msgs::pose
-// 단위 변환 : mm --> m 
-// publish topci : "/mavros/vision_pose/pose", type: geometry_mssgs::poseStamped
 
-// publish 노드 생성 
+// 전역에 new PoseStamped 메시지 생성 
+geometry_msgs::PoseStamped receivedMsg;
+
 ros::Publisher pose_pub;
 
-//callback 함수 구현 
-void poseSubCallback(const geometry_msgs::Pose msg){
-
-    geometry_msgs::PoseStamped receivedMsg; 
+void poseSubCallback(const geometry_msgs::Pose msg)
+{
+    ROS_INFO("Subscribe pose");
     receivedMsg.pose = msg;
-    
-    //단위 변환 mm -> m 
+
+// mm --> m
     receivedMsg.pose.position.x = receivedMsg.pose.position.x/1000;
     receivedMsg.pose.position.y = receivedMsg.pose.position.y/1000;
     receivedMsg.pose.position.z = receivedMsg.pose.position.z/1000;
-    
-    // 부가 정보
+
+// 부가 정보
     receivedMsg.header.seq++;
     receivedMsg.header.stamp = ros::Time::now();
 
-    //publish 
+//    ROS_INFO("*** pose_receiver position ***  x : %f, y: %f, z: %f",receivedMsg.pose.position.x ,receivedMsg.pose.position.y, receivedMsg.pose.position.z);
     pose_pub.publish(receivedMsg);
-    ROS_INFO("seq: %d, publish PoseStamped to MAVROS",receivedMsg.header.seq);
+    ROS_INFO("seq: %d, publish PoseStamped",receivedMsg.header.seq);   
 
 }
 
 
-int main(int argc, char **argv){
-    //노드 초기화   
+
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "pose_receiver");
-    //노드 핸들 생성
-    ros::NodeHandle nh;
-    // subscribe 노드 생성 
-    ros::subscriber pose_sub = nh.subscribe("hd_indoor/pose",1000, poseSubCallback);
-    nh.advertise<geometry_msgs::PoseStamped>("/mavros/vision_pose/pose", 1000); 
+    ros::NodeHandle n;
+
+    ros::Subscriber pose_sub = n.subscribe("hd_indoor/pose", 1000, poseSubCallback);
+    
+    pose_pub = n.advertise<geometry_msgs::PoseStamped>("/mavros/vision_pose/pose", 1000);
 
     ros::spin();
-
     return 0;
-
 }
